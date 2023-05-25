e6b30cb09470d6ce88cdce2a126ab36ab267d4cc
Joao Zavisas <133046097+Zavisas@users.noreply.github.com>
2023-05-25 15:58:15 -0300

    ImprimaMaior

---
+++ src/ImprimaMaior.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ImprimaMaior {
+    public static void main(String[] args) {
+        main();
+    }
+
+    public static void main(){
+        Scanner ler = new Scanner(System.in);
+
+        System.out.print("Digite o 1° valor: ");
+        int v1 = ler.nextInt();
+        System.out.print("Digite o 2° valor: ");
+
+        int v2 = ler.nextInt();
+        System.out.println(v1);
+        System.out.println(v2);
+
+        if (v1 > v2) {
+            System.out.println("MAIOR");
+        }
+        else if (v1 == v2){
+                System.out.println("IGUAIS");
+            }else {
+            System.out.println(v1 + " é MENOR");
+        }
+        }
+    }
